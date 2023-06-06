a2a1bb5d02b23e627656b6a3d971ed1451f2557f
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-06-06 19:56:20 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,6 @@
+cek = '# CEK RESULT CRACK'
+	sol().print(mark(cek, style='green'))
+	kayes = '[01] Cek Hasil Cp\n[02] Cek Hasil Ok\n[00] Kembali Ke Menu'
+	kis = nel(kayes, style='cyan')
+	cetak(nel(kis, title='RESULTS'))
+	kz = input(x+'['+p+'f'+x+'] Pilih : ')

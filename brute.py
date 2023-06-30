11cb06ae57e41a141247b3cae25828d35f2ef760
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-06-30 22:27:22 +0700

    Update brute.py

--- brute.py
+++ brute.py
@@ -10,6 +10,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Indonesia" == fc:
-		__import__("brute").main_()
+		__import__("brute").main()
 	else:
-		__import__("brute").main_()
+		__import__("brute").main()
